0a26bb5679175e12062d5a76cada9a31cc3777c8
shashinherath <shashin.malinda@gmail.com>
2024-05-10 14:31:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import TeacherPortal from './pages/TeacherPortal';
+import StudentPortal from './pages/StudentPortal';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teacher" element={<TeacherPortal />} />
+        <Route path="/student" element={<StudentPortal />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
