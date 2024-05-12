85885634b848b51f623fdddf0d769b1606ee67ca
shashinherath <shashin.malinda@gmail.com>
2024-05-12 15:04:48 +0530

    Fix nested route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import TeacherPortal from './pages/TeacherPortal';
-import StudentPortal from './pages/StudentPortal';
-import Login from './components/Login';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import TeacherPortal from "./pages/TeacherPortal";
+import StudentPortal from "./pages/StudentPortal";
+import Login from "./components/Login";
+import HomeContent from "./components/HomeContent";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomeContent />} />
+          <Route path="login" element={<Login />} />
+        </Route>
         <Route path="/teacher" element={<TeacherPortal />} />
         <Route path="/student" element={<StudentPortal />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,6 @@ import Footer from "./Footer";
 function Login() {
   return (
     <>
-      <Navbar />
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img className="mx-auto h-10 w-auto" src="" alt="EduConnect Pro" />
@@ -87,7 +86,6 @@ function Login() {
           </p>
         </div>
       </div>
-      <Footer />
     </>
   );
 }
