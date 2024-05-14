9650432d1f1d66291d9f15fdb9e7dfb4acb009e0
shashinherath <shashin.malinda@gmail.com>
2024-05-14 23:07:46 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,11 +15,11 @@ import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: InboxIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
+  { name: "AI Portal", href: "#", icon: UsersIcon, current: false },
+  { name: "Message", href: "#", icon: FolderIcon, current: false },
+  { name: "", href: "#", icon: CalendarIcon, current: false },
+  { name: "", href: "#", icon: InboxIcon, current: false },
+  { name: "", href: "#", icon: ChartBarIcon, current: false },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
