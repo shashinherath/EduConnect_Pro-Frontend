456f203141935bbb2d646d66c8ea7892250efad6
shashinherath <shashin.malinda@gmail.com>
2024-05-26 15:03:28 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import Footer from "../components/H_Footer";
+import Navbar from "../components/H_Navbar";
 import { Outlet } from "react-router-dom";
 
 function Home() {
