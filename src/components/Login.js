d46b2de8a3ec8b22194398cf1c7160d66f2e3ece
shashinherath <shashin.malinda@gmail.com>
2024-05-26 15:03:49 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "./H_Navbar";
+import Footer from "./H_Footer";
 
 function Login() {
   return (
