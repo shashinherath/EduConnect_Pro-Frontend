91caa1458dd9afb5ad457fcae913a7b7571361ab
shashinherath <shashin.malinda@gmail.com>
2024-05-27 23:57:35 +0530

    Update Ad_Login.js

--- src/components/Ad_Login.js
+++ src/components/Ad_Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Ad_Login() {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gray-100">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               className="mx-auto h-10 w-auto"
