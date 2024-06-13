1728a86c3e4114c137461441e484249d97a08be2
shashinherath <shashin.malinda@gmail.com>
2024-06-13 14:55:55 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/H_Footer";
-import Navbar from "../components/H_Navbar";
+import Navbar from "../components/H_Nav_with_Hero";
 import { Outlet } from "react-router-dom";
 
 function Home() {
+  useEffect(() => {
+    document.title = "EduConnect Pro";
+  }, []);
+
   return (
     <>
       <Navbar />
