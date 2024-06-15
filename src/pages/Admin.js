c9938c0bd67714b1d6710a7cf95a82025cdc0c49
shashinherath <shashin.malinda@gmail.com>
2024-06-16 00:29:33 +0530

    Update Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Admin() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (window.location.pathname === '/admin') {
+      navigate('/admin/dashboard');
+    }
+  }, []);
+
   return (
     <Outlet />
   )
