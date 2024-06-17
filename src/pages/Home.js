e2b3eb90d19a660c9b86d97d0b7f05a128d3a408
shashinherath <shashin.malinda@gmail.com>
2024-06-17 21:40:15 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import Footer from "../components/H_Footer";
 import Navbar from "../components/H_Nav_with_Hero";
+import H_Team from "../components/H_Team";
+import H_Content from "../components/H_Content";
 import { Outlet } from "react-router-dom";
 
 function Home() {
@@ -11,7 +13,8 @@ function Home() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <H_Content />
+      <H_Team />
       <Footer />
     </>
   );
