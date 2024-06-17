242017d60542997be580c0b4adff9faddcafaa41
shashinherath <shashin.malinda@gmail.com>
2024-06-17 21:40:05 +0530

    Update Ad_Admins.js

--- src/components/Ad_Admins.js
+++ src/components/Ad_Admins.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const people = [
   {
@@ -23,17 +24,18 @@ const people = [
 ]
 
 export default function Ad_Admins() {
+  const navigate = useNavigate();
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Users</h1>
+          <h1 className="text-xl font-semibold text-gray-900"></h1>
           <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title, email, and role.
           </p>
         </div>
         <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
+          <button onClick={() => {navigate('add')}}
             type="button"
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
           >
