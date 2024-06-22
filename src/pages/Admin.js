22d82b96fefeb785267354131cc21922edd9fbb1
shashinherath <shashin.malinda@gmail.com>
2024-06-22 15:44:08 +0530

    Update Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { SearchProvider } from "../components/SearchContext";
 
 function Admin() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (window.location.pathname === '/admin') {
-      navigate('/admin/dashboard');
+    if (window.location.pathname === "/admin") {
+      navigate("/admin/dashboard");
     }
   }, []);
 
   return (
-    <Outlet />
-  )
+    <SearchProvider>
+      <Outlet />
+    </SearchProvider>
+  );
 }
 
-export default Admin
+export default Admin;
