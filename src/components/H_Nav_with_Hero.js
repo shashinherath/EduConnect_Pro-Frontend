0f7d09f8ff6c728a066828214f5a3efcdb550faa
Savindu Dinul Mithsuka <savindu.socialmedia@gmail.com>
2024-06-26 03:27:29 +0530

    Update H_Nav_with_Hero.js
    Discretional text added

--- src/components/H_Nav_with_Hero.js
+++ src/components/H_Nav_with_Hero.js
@@ -128,7 +128,7 @@ export default function H_Nav_with_Hero() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-              Announcing our next round of funding.{' '}
+            Discover the Future of Education with EduConnect Pro.{' '}
               <a href="#" className="font-semibold text-white">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
@@ -141,7 +141,7 @@ export default function H_Nav_with_Hero() {
               
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-            "Welcome to EduConnect Pro, a cutting-edge Learning Management System (LMS) designed to revolutionize education by seamlessly connecting students and educators across borders. With advanced features for personalized learning, interactive collaboration, and robust communication tools, EduConnect Pro enhances the educational experience and fosters a global community of learning and growth."
+            "Welcome to EduConnect Pro, a cutting-edge Learning Management System designed to revolutionize education by seamlessly connecting students and educators across borders. With advanced features for personalized learning, interactive collaboration, and robust communication tools, EduConnect Pro enhances the educational experience and fosters a global community of learning and growth."
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
