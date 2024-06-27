8a4e2ab5bcf91af70a01d3c4b9e51942eb3331cc
shashinherath <shashin.malinda@gmail.com>
2024-06-27 15:49:00 +0530

    Update H_Nav_with_Hero.js

--- src/components/H_Nav_with_Hero.js
+++ src/components/H_Nav_with_Hero.js
@@ -1,24 +1,23 @@
 /* This example requires Tailwind CSS v3.0+ */
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import Hero_video from '../assets/Hero.mp4'
-import { Link } from 'react-router-dom'
-import home_logo from '../assets/logo/Owl only Transparant.png'
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Hero_video from "../assets/Hero.mp4";
+import { Link } from "react-router-dom";
+import home_logo from "../assets/logo/Owl only Transparant.png";
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+  { name: "Home", href: "/" },
+  { name: "Team", href: "#team" },
+  { name: "About", href: "#" },
+  { name: "Partnership", href: "#" },
+];
 
 export default function H_Nav_with_Hero() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className="relative isolate overflow-hidden bg-gray-900">
-
       <video
         autoPlay
         loop
@@ -54,12 +53,15 @@ export default function H_Nav_with_Hero() {
         </svg>
       </div>
       <div className="px-6 lg:px-8">
-        <nav className="flex items-center justify-between pt-6" aria-label="Global">
+        <nav
+          className="flex items-center justify-between pt-6"
+          aria-label="Global"
+        >
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">EduConnect Pro</span>
               <img className="h-20" src={home_logo} alt="" />
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -73,24 +75,34 @@ export default function H_Nav_with_Hero() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
+              <Link
+                key={item.name}
+                to={item.href}
+                className="text-sm font-semibold leading-6 text-white"
+              >
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link to={'/login'} className="text-sm font-semibold leading-6 text-white">
+            <Link
+              to={"/login"}
+              className="text-sm font-semibold leading-6 text-white"
+            >
               Log in <span aria-hidden="true">&rarr;</span>
             </Link>
           </div>
         </nav>
         <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <Dialog.Panel focus="true" className="fixed inset-0 z-10 overflow-y-auto bg-gray-900 px-6 py-6 lg:hidden">
+          <Dialog.Panel
+            focus="true"
+            className="fixed inset-0 z-10 overflow-y-auto bg-gray-900 px-6 py-6 lg:hidden"
+          >
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">EduConnect Pro</span>
                 <img className="h-8" src={home_logo} alt="" />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-400"
@@ -104,18 +116,18 @@ export default function H_Nav_with_Hero() {
               <div className="-my-6 divide-y divide-gray-500/25">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
-                      href={item.href}
+                      to={item.href}
                       className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white hover:bg-gray-400/10"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
                 <div className="py-6">
                   <Link
-                    to={'/login'}
+                    to={"/login"}
                     className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-white hover:bg-gray-400/10"
                   >
                     Log in
@@ -137,22 +149,18 @@ export default function H_Nav_with_Hero() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Connecting Beyond Borders
-              
+              Connecting Beyond Borders
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300">
             "Welcome to EduConnect Pro, a cutting-edge Learning Management System designed to revolutionize education by seamlessly connecting students and educators across borders. With advanced features for personalized learning, interactive collaboration, and robust communication tools, EduConnect Pro enhances the educational experience and fosters a global community of learning and growth."
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
+              <Link
+                to="/login"
                 className="rounded-md bg-indigo-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
-                Get started
-              </a>
-              <a href="#" className="text-base font-semibold leading-7 text-white">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+                Log in <span aria-hidden="true">&rarr;</span>
+              </Link>
             </div>
           </div>
         </div>
@@ -183,5 +191,5 @@ export default function H_Nav_with_Hero() {
         </svg>
       </div>
     </div>
-  )
+  );
 }
