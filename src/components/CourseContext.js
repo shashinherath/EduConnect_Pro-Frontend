eca250119aabedfa71ab97c1dbff3cbcf7c23fa0
shashinherath <shashin.malinda@gmail.com>
2024-06-28 22:55:51 +0530

    Update CourseContext.js

--- src/components/CourseContext.js
+++ src/components/CourseContext.js
@@ -1,10 +1,22 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const CourseContext = createContext();
 
 export const CourseProvider = ({ children }) => {
   const [materialCourseId, setMaterialCourseId] = useState("");
   const [materialCourseTitle, setMaterialCourseTitle] = useState("");
+
+  useEffect(() => {
+    const storedId = localStorage.getItem("materialCourseId");
+    setMaterialCourseId(storedId);
+    const storedTitle = localStorage.getItem("materialCourseTitle");
+    setMaterialCourseTitle(storedTitle);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("materialCourseId", materialCourseId);
+    localStorage.setItem("materialCourseTitle", materialCourseTitle);
+  }, [materialCourseId, materialCourseTitle]);
 
   return (
     <CourseContext.Provider
