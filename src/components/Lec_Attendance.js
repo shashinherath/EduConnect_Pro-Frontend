48466b5605edd0e2d86cf645051c3036106006b1
shashinherath <shashin.malinda@gmail.com>
2024-06-29 16:24:55 +0530

    Update Lec_Attendance.js

--- src/components/Lec_Attendance.js
+++ src/components/Lec_Attendance.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Lec_Attendance() {
-  return <div>Lec_Attendance</div>;
+  return (
+    <div className="grid h-screen place-items-center">
+      <h1 className="text-center font-bold text-7xl -translate-y-32">
+        Extra/Additional feature
+      </h1>
+    </div>
+  );
 }
