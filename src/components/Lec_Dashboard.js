0b52f0cea6a74aecb99b0b68c6adb60c2746466c
shashinherath <shashin.malinda@gmail.com>
2024-07-07 03:06:07 +0530

    Fix screen size

--- src/components/Lec_Dashboard.js
+++ src/components/Lec_Dashboard.js
@@ -267,7 +267,7 @@ export default function Lec_Dashboard() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col md:pl-64">
+        <div className="flex flex-col md:pl-64 min-h-screen">
           <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
             <button
               type="button"
@@ -366,7 +366,7 @@ export default function Lec_Dashboard() {
             </div>
           </div>
 
-          <main className="flex-1 min-h-screen bg-gray-200">
+          <main className="flex-1 bg-gray-200 h-full">
             <div className="py-6">
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 <h1 className="text-4xl font-extrabold text-gray-900 text-left">
