5390ca3ba28002185929c2a5d14d223746209cf3
shashinherath <shashin.malinda@gmail.com>
2024-07-20 19:49:00 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,18 @@ module.exports = {
     extend: {
       animation: {
         "fade-in": "fadeIn 5s ease-in-out forwards",
+        wave1: "wave 1.3s linear infinite",
+        wave2: "wave 1.3s linear infinite -1.1s",
+        wave3: "wave 1.3s linear infinite -0.9s",
       },
       keyframes: {
         fadeIn: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
+        },
+        wave: {
+          "0%, 60%, 100%": { transform: "initial" },
+          "30%": { transform: "translateY(-15px)" },
         },
       },
     },
