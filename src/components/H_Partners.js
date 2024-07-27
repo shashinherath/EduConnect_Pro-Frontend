ce8c9a1ed402448cf0e9a02b0136f9d8ce32af97
shashinherath <shashin.malinda@gmail.com>
2024-07-28 01:46:22 +0530

    Update H_Partners.js

--- src/components/H_Partners.js
+++ src/components/H_Partners.js
@@ -1,6 +1,7 @@
 import Logo from "../assets/logo/Owl only Transparant.png";
 import VLE from "../assets/logo/VLE.png";
 import UWU from "../assets/logo/UWU_Logo.png";
+import OpenAI from "../assets/logo/OpenAI_Logo.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
@@ -9,7 +10,7 @@ export default function H_Partners() {
   useEffect(() => {
     AOS.init();
   }, []);
-  
+
   return (
     <div id="partnership" className="bg-gray-300">
       <div className="mx-auto max-w-7xl py-16 px-6 sm:py-20 lg:px-8">
@@ -37,7 +38,7 @@ export default function H_Partners() {
               data-aos-anchor-placement="center-bottom"
               className="mt-4 ml-8 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0"
             >
-              <img className="h-14 lg:h-24" src={VLE} alt="VLE" />
+              <img className="h-14 lg:h-24" src={OpenAI} alt="VLE" />
             </div>
             <div
               data-aos="fade-up"
